bad84957dd800e63477df56bddcb2e770a039a77
SolidJuho <juho.vatanen@gmail.com>
2017-08-31 10:23:19 +0300

    Added player char

--- Multiplayer Fps/Assets/Scripts/GameManager.cs
+++ Multiplayer Fps/Assets/Scripts/GameManager.cs
@@ -3,6 +3,25 @@ using UnityEngine;
 
 public class GameManager : MonoBehaviour {
 
+    public static GameManager instance;
+
+    public MatchSettings matchSettings;
+
+    private void Awake()
+    {
+        if (instance != null)
+        {
+            Debug.LogError("More than one Gamemanager in scene");
+        }
+        else
+        {
+            instance = this;
+        }
+
+    }
+
+    #region Player Tracking
+
     private const string PLAYER_ID_PREFIX = "Player ";
 
     private static Dictionary<string, Player> players = new Dictionary<string, Player>();
@@ -37,4 +56,7 @@ public class GameManager : MonoBehaviour {
     //    GUILayout.EndVertical();
     //    GUILayout.EndArea();
     //}
+
+#endregion 
+
 }
--- Multiplayer Fps/Assets/Scripts/Player.cs
+++ Multiplayer Fps/Assets/Scripts/Player.cs
@@ -69,7 +69,7 @@ public class Player : NetworkBehaviour {
 
     private IEnumerator Respawn()
     {
-        yield return new WaitForSeconds(3f);
+        yield return new WaitForSeconds(GameManager.instance.matchSettings.respawnTime);
 
         SetDefaults();
         Transform _spawnPoint = NetworkManager.singleton.GetStartPosition();
